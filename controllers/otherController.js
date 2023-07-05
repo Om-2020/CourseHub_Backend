7f62e83a099972f8378103d552fd58251d0329c7
Om-2020 <omprakashbharti1123@gmail.com>
2023-07-05 18:11:48 +0530

    second change

--- controllers/otherController.js
+++ controllers/otherController.js
@@ -10,7 +10,7 @@ export const contact = catchAsyncError(async (req, res, next) => {
     return next(new ErrorHandler("All fields are mandatory", 400));
 
   const to = process.env.MY_MAIL;
-  const subject = "Contact from CourseBundler";
+  const subject = "Contact from CourseHub";
   const text = `I am ${name} and my Email is ${email}. \n${message}`;
 
   await sendEmail(to, subject, text);
@@ -27,7 +27,7 @@ export const courseRequest = catchAsyncError(async (req, res, next) => {
     return next(new ErrorHandler("All fields are mandatory", 400));
 
   const to = process.env.MY_MAIL;
-  const subject = "Requesting for a course on CourseBundler";
+  const subject = "Requesting for a course on CourseHub";
   const text = `I am ${name} and my Email is ${email}. \n${course}`;
 
   await sendEmail(to, subject, text);
--- controllers/userController.js
+++ controllers/userController.js
@@ -175,6 +175,7 @@ export const resetPassword = catchAsyncError(async (req, res, next) => {
       $gt: Date.now(),
     },
   });
+  console.log(user);
 
   if (!user)
     return next(new ErrorHandler("Token is invalid or has been expired", 401));
@@ -298,11 +299,11 @@ export const deleteMyProfile = catchAsyncError(async (req, res, next) => {
     });
 });
 
-/* User.watch().on("change", async () => {
+/*User.watch().on("change", async () => {
   const stats = await Stats.find({}).sort({ createdAt: "desc" }).limit(1);
 
   const subscription = await User.find({ "subscription.status": "active" });
-  stats[0].users = await User.countDocuments();
+  stats[0].users = Await User.countDocuments();
   stats[0].subscription = subscription.length;
   stats[0].createdAt = new Date(Date.now());
 
